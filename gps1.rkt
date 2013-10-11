68aa81ff67332fa4135195c8942b45defb55fdaf
Stephen De Gabrielle <spdegabrielle@users.noreply.github.com>
2013-10-11 16:54:10 +0100

    Update gps1.rkt

--- gps1.rkt
+++ gps1.rkt
@@ -1,5 +1,4 @@
 #lang racket
-;GPS program was written by Newell Simon and Shaw in 1960
 ;;; -*- Mode: Lisp; Syntax: Common-Lisp; -*-
 ;;; Code from Paradigms of Artificial Intelligence Programming
 ;;; Copyright (c) 1991 Peter Norvig
@@ -22,7 +21,7 @@
 (define *state* null) ; "The current state: a list of conditions."
 (define *ops* null) ; "A list of available operators."
 
-(struct op (action preconds add-list del-list )) ;"An operation"
+(struct op (action preconds add-list del-list)) ;"An operation"
 
   ;"General Problem Solver: achieve all goals using *ops*."
 (define (GPS *state* goals *ops*)
@@ -35,7 +34,7 @@
       (ormap apply-op ; ormap replaced some
             (find-all goal *ops* appropriate-p))))
 
-(defun (find-all item seq &rest keyword-args &key (test #'eql) test-not &allow-other-keys)
+(defun (find-all item seq . keyword-args &key (test #'eql) test-not)
         (if test-not
                 (apply remove item seq
                         :test-not (complement test-not) keyword-args)
@@ -49,13 +48,13 @@
 (define (apply-op op)
   ;"Print a message and update *state* if op is applicable."
   (when (every #'achieve (op-preconds op))
-    (print (list 'executing (op-action op)))
+    (displayln (list 'executing (op-action op)))
     (set! *state* (set-difference *state* (op-del-list op)))
     (set! *state* (union *state* (op-add-list op)))
     t))
 
 ;;; ==============================
-;(struct op (action   preconds add-list del-list )) ;"An operation"
+;(struct op (action                   preconds                  add-list          del-list )) ;"An operation"
 (define *school-ops*
   (list
     (make-op 'drive-son-to-school    '(son-at-home car-works) '(son-at-school) '(son-at-home))
@@ -64,3 +63,5 @@
     (make-op 'telephone-shop         '(know-phone-number) '(in-communication-with-shop))
     (make-op 'look-up-number         '(have-phone-book) '(know-phone-number))
     (make-op 'give-shop-money        '(have-money) '(shop-has-money) '(have-money))))
+    
+    
